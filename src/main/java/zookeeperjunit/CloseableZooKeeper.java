5ae28ff9909d4122bb12cd32dcb2166385aa598d
Peter Nerg <peter.nerg@gmail.com>
2016-01-09 08:22:23 +0100

    #10 cleanup and javadoc to CloseableZooKeeper

--- src/main/java/zookeeperjunit/CloseableZooKeeper.java
+++ src/main/java/zookeeperjunit/CloseableZooKeeper.java
@@ -15,27 +15,78 @@
  */
 package zookeeperjunit;
 
+import static javascalautils.TryCompanion.Try;
+
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
+import javascalautils.Try;
 import javascalautils.Unit;
-
-import static javascalautils.TryCompanion.Try;
 
 /**
  * Extends the standard {@link ZooKeeper} connection class with the {@link Closeable} interface. <br>
  * This is to be able to use it in try-with-resources clauses.
  * 
  * @author Peter Nerg
+ * @since 1.0
  */
 final class CloseableZooKeeper extends ZooKeeper implements Closeable {
 	CloseableZooKeeper(String connectString, int sessionTimeout, Watcher watcher) throws IOException {
 		super(connectString, sessionTimeout, watcher);
 	}
 
+	/**
+	 * Delete the specified node and all of it's children.
+	 * @param path
+	 *            The path
+	 */
+	Try<Unit> deleteRecursively(String path) {
+		Try<Stream<String>> children = getChildren(path);
+		getChildren(path).forEach(stream -> {
+			stream.forEach(child -> {
+				String joinedZNode = path.equals("/") ? path + child : path + "/" + child;
+				deleteRecursively(joinedZNode);
+			});
+		});
+		// the flatMap/delete only executes if the getChildren is a Success
+		// i.e. no point in deleting a path that doesn't exist or ZK is down
+		return children.flatMap(s -> delete(path));
+	}
+
+	/**
+	 * Attempts to delete the provided path. <br>
+	 * Will fail if the node/path does not exist or ZK is down.
+	 * 
+	 * @param path
+	 *            The path
+	 * @return The result
+	 */
+	private Try<Unit> delete(String path) {
+		return Try(() -> {
+			delete(path, -1); // -1 for ANY version
+			return Unit.Instance;
+		});
+	}
+
+	/**
+	 * Attempts to get all the children of the path. <br>
+	 * Will fail if the node/path does not exist or ZK is down.
+	 * 
+	 * @param path
+	 *            The path
+	 * @return The result
+	 */
+	private Try<Stream<String>> getChildren(String path) {
+		return Try(() -> getChildren(path, null).stream());
+	}
+
+	/**
+	 * Just invokes {@link ZooKeeper#close()} to shutdown the ZK instance.
+	 */
 	@Override
 	public void close() {
 		Try(() -> {
