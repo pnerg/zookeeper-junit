70c45c5b13ecde75567ee2b43591b543eee17371
Peter Nerg <peter.nerg@gmail.com>
2016-01-16 11:01:22 +0100

    made CloseableZooKeeper public

--- src/main/java/zookeeperjunit/CloseableZooKeeper.java
+++ src/main/java/zookeeperjunit/CloseableZooKeeper.java
@@ -38,16 +38,18 @@ import javascalautils.concurrent.Future;
  * This is to be able to use it in try-with-resources clauses.
  * 
  * @author Peter Nerg
- * @since 1.0
+ * @since 1.1
  */
-final class CloseableZooKeeper extends ZooKeeper implements Closeable {
+public final class CloseableZooKeeper extends ZooKeeper implements Closeable {
 	CloseableZooKeeper(String connectString, int sessionTimeout, Watcher watcher) throws IOException {
 		super(connectString, sessionTimeout, watcher);
 	}
 
 	/**
 	 * Creates a ZooKeeper connection.
-	 * @param connectString The connect string
+	 * 
+	 * @param connectString
+	 *            The connect string
 	 * @return A Future which will be completed once the connections is properly made and we're connected.
 	 */
 	static Future<CloseableZooKeeper> connect(String connectString) {
@@ -58,29 +60,37 @@ final class CloseableZooKeeper extends ZooKeeper implements Closeable {
 					latch.countDown();
 				}
 			});
-			latch.await(); //this blocks indefinitely
+			latch.await(); // this blocks indefinitely
 			return zk;
 		});
 	}
-	
+
 	/**
 	 * Creates a ZooKeeper connection.
-	 * @param connectString The connect string
-	 * @param duration The duration to wait for a connection to be established
+	 * 
+	 * @param connectString
+	 *            The connect string
+	 * @param duration
+	 *            The duration to wait for a connection to be established
 	 * @return The connection
-	 * @throws TimeoutException If exceed the provided duration
-	 * @throws Throwable Any other issue
+	 * @throws TimeoutException
+	 *             If exceed the provided duration
+	 * @throws Throwable
+	 *             Any other issue
 	 */
 	static CloseableZooKeeper blockingConnect(String connectString, Duration duration) throws TimeoutException, Throwable {
 		return connect(connectString).result(duration);
 	}
 
 	/**
-	 * Delete the specified node and all of it's children.
+	 * Recursively deletes the specified node and all of it's children. Will fail if the node/path does not exist or ZK is down.
+	 * 
 	 * @param path
 	 *            The path
+	 * @return The result of the operation
+	 * @since 1.1
 	 */
-	Try<Unit> deleteRecursively(String path) {
+	public Try<Unit> deleteRecursively(String path) {
 		Try<Stream<String>> children = getChildren(path);
 		getChildren(path).forEach(stream -> {
 			stream.forEach(child -> {
@@ -99,10 +109,11 @@ final class CloseableZooKeeper extends ZooKeeper implements Closeable {
 	 * 
 	 * @param path
 	 *            The path
-	 * @return The result
+	 * @return The result of the operation
+	 * @since 1.1
 	 */
-	private Try<Unit> delete(String path) {
-		return Try(() -> delete(path, -1) ); // -1 for ANY version
+	public Try<Unit> delete(String path) {
+		return Try(() -> delete(path, -1)); // -1 for ANY version
 	}
 
 	/**
@@ -111,14 +122,17 @@ final class CloseableZooKeeper extends ZooKeeper implements Closeable {
 	 * 
 	 * @param path
 	 *            The path
-	 * @return The result
+	 * @return The result of the operation
+	 * @since 1.1
 	 */
-	private Try<Stream<String>> getChildren(String path) {
+	public Try<Stream<String>> getChildren(String path) {
 		return Try(() -> getChildren(path, null).stream());
 	}
 
 	/**
-	 * Just invokes {@link ZooKeeper#close()} to shutdown the ZK instance.
+	 * Just invokes {@link ZooKeeper#close()} to shutdown the ZK instance. <br>
+	 * Any errors during shutdown are ignored.
+	 * @since 1.1
 	 */
 	@Override
 	public void close() {
